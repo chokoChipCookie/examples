3e37b9f2a43be1554cc43b4cdfba2694ae4b3161
solheek <sh1025b@gmail.com>
2021-03-16 01:18:33 +0900

    Add vue project

---
+++ solheek/vue-project-01/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <PostList />
+  </div>
+</template>
+
+<script>
+import PostList from './components/PostList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PostList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
