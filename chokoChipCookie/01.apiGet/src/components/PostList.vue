d5bc01cda756b163f427c7cbd764df5dc18e33f0
chokoChipCookie <80441858+chokoChipCookie@users.noreply.github.com>
2021-03-21 10:03:13 +0900

    Add files via upload

---
+++ chokoChipCookie/01.apiGet/src/components/PostList.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <table id="post-list">
+            <tr v-for="post in posts" :key="post.id">
+                <td> {{ post.id }}</td>
+                <td> {{ post.userId }}</td>
+                <td> {{ post.title }}</td>
+                <td> {{ post.body }}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: "PostList",
+        data(){
+            return{
+                posts:[]
+            }
+        },
+        mounted(){
+            this.getApi();
+        },
+        methods: {
+            getApi(){
+                const url = 'https://jsonplaceholder.typicode.com/posts';
+                axios.get(url)
+                .then((res) =>{
+                    // console.log(res);
+                    this.posts = res.data;
+                });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
